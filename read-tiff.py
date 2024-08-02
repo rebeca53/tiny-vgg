089d516e6750e5ce526ed23aad39041d6dda1c88
Rebeca Nunes <rebeca.n.rod@gmail.com>
2024-08-02 14:13:35 +0200

    reading tiff file successfully

--- read-tiff.py
+++ read-tiff.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 tiny_vgg = tf.keras.models.load_model('trained_vgg_best.h5')
 WIDTH = 64
 HEIGHT = 64
-test_image = './Forest_1.tif'
+test_image = './Forest_1465.tif'
 
 # attempt with tensorflow.io
 # img loaded has all pixels equal to 4
@@ -24,10 +24,12 @@ test_image = './Forest_1.tif'
 
 # attempt with tifffile -- IN PROGRESS
 # it imports all channels, but it display nothing!
-# import tifffile
-# img = tifffile.imread(test_image)
-# print(img[0])
-# img = img[:,:,:3] # Remove extra channel
+import tifffile
+img = tifffile.imread(test_image, key=0) # use the key=0
+print(img.shape)
+print(img.dtype)
+# img = img[:,:,1:4] # get B02, B03, B04 - BGR
+img = img[:, :, [3, 2, 1]] # change order to have RGB
 
 # attempt with rasterio -- IN PROGRESS
 # I can't install it!
@@ -36,7 +38,7 @@ test_image = './Forest_1.tif'
 
 fig = plt.figure()
 fig.add_subplot(1,2,1)
-plt.imshow(img)
+plt.imshow(img.astype('uint8'))
 
 img = tf.image.convert_image_dtype(img, tf.float32)
 img = tf.image.resize(img, [WIDTH, HEIGHT])
@@ -49,13 +51,7 @@ pred_label = class_names[np.argmax(np.round(img_predictions,2))]
 print(" Predicted label is :: "+ pred_label)
 
 print(img.shape)
-
-
-
-
-
-
-
+print(img.dtype)
 
 
 img = tf.io.read_file('./data/class_10_val/map_images/Forest_1465.JPEG')
@@ -75,6 +71,8 @@ img = tf.expand_dims(img, 0)  # Create batch axis
 
 # img_predictions = tiny_vgg.predict(img)
 print(img.shape)
+print(img.dtype)
+
 
 img_predictions = tiny_vgg.predict(img)
 class_names = ["highway", "forest", "river", "permanent crop", "industrial", "annual crop", "sea or lake", "herbaceous", "residential", "pasture"]
